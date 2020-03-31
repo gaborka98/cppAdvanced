ae7421d5a1e559d8757b974993def162f0a49584
Horváth Gábor <gaborka812@gmail.com>
2020-03-31 11:31:24 +0200

    Initial Commit

---
+++ cppAdvanced/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppAdvanced
+//
+//  Created by Gábor Horváth on 2020. 03. 31..
+//  Copyright © 2020. Gábor Horváth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
